c13774dd9eced42f14f460eae555fe4746a8f5b8
mariaakhmadieva <mariaakhmadieva@yandex.ru>
2022-08-15 21:09:01 +0500

    ex2/3/4

--- lesson02/ex1/Program.cs
+++ lesson02/ex1/Program.cs
@@ -5,19 +5,20 @@
     if (arg3 > result) result = arg3;
     return result;
 }
-int a1 = 15;
+int a1 = 1533;
 int b1 = 21;
 int c1 = 39;
 int a2 = 12;
-int b2 = 23;
+int b2 = 23556;
 int c2 = 33;
 int a3 = 13;
 int b3 = 231;
 int c3 = 313;
 
-int max1 = Max(a1, b1, c1);
-int max2 = Max(a2, b2, c2);
-int max3 = Max(a3, b3, c3);
-int max = Max(max1, max3, max3);
+/// int max1 = Max(a1, b1, c1);
+/// int max2 = Max(a2, b2, c2);
+/// int max3 = Max(a3, b3, c3);
+
+int max = Max(Max(a1, b1, c1), Max(a2, b2, c2), Max(a3, b3, c3));
 
 Console.WriteLine(max);
