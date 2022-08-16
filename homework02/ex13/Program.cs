e562d8c148f47565a1d2f7ff823f9923ef4cb8d6
mariaakhmadieva <mariaakhmadieva@yandex.ru>
2022-08-16 07:04:04 +0500

    ex13 and ex15 - done

---
+++ homework02/ex13/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите Ваше число ");
+int val = Convert.ToInt32(Console.ReadLine());
+if (val > 99)
+{
+    char[] array = val.ToString().ToCharArray();
+    Console.WriteLine(array[2]);
+}
+else
+{
+    Console.WriteLine("Третьей цифры нет");
+};
---
+++ homework02/ex15/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Ведите число от 1 до 7");
+int val = Convert.ToInt32(Console.ReadLine());
+if (val == 6 || val == 7)
+{
+    Console.WriteLine("День является выходным");
+}
+else
+{
+    Console.WriteLine("День не является выходным");
+};
