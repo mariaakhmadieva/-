5bdd5024fe738b718f81de8bcda0f61ea5ce162f
mariaakhmadieva <mariaakhmadieva@yandex.ru>
2022-08-23 15:55:43 +0500

    ex27-done

--- homework04/ex27/Program.cs
+++ homework04/ex27/Program.cs
@@ -3,15 +3,22 @@
 // 82 -> 10
 // 9012 -> 12
 
-Console.WriteLine("Введите число: ");
+Console.Write("Введите число: ");
 int num = Convert.ToInt32(Console.ReadLine());
-char[] arr = num.ToString().ToCharArray();
-
-
-int sum = 0;
-while (arr[i] <= arr.Length);
+int SumNum(int number)
 {
-    sum = arr[i] + sum;
-    i++;
-}
-Console.WriteLine(sum);
+    int counter = Convert.ToString(number).Length;
+    int advance = 0;
+    int result = 0;
+
+    for (int i = 0; i < counter; i++)
+    {
+      advance = number - number % 10;
+      result = result + (number - advance);
+      number = number / 10;
+    }
+   return result;
+  }
+  
+int sumNum = SumNum(num);
+Console.WriteLine("Сумма элементов равна: " + sumNum);
