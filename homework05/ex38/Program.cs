3c9ca3fbf9c940344a9b9f4551829414b11896f0
mariaakhmadieva <mariaakhmadieva@yandex.ru>
2022-08-31 03:08:21 +0500

    ex38-done

--- homework05/ex38/Program.cs
+++ homework05/ex38/Program.cs
@@ -1,3 +1,19 @@
 ﻿// Задайте массив вещественных чисел. 
 // Найдите разницу между максимальным и минимальным элементов массива.
 // [3 7 22 2 78] -> 76
+
+
+double [] arr = new double[10];
+
+for (int i = 0; i < arr.Length; i++)
+{
+    arr[i] = new Random().NextDouble();
+    Console.Write(" " + arr[i]);
+}
+double max = arr.Max();
+double min = arr.Min();
+
+double diff = max - min;
+
+Console.WriteLine($"\n Максимальный элемент: {max}\n Минимальный элемент: {min}");
+Console.WriteLine($"Разница между максимальным и минимальным элементом: " + diff);
