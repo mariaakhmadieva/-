8beaa497a3b9d8414d64bc9a1fe36aa8b959264d
mariaakhmadieva <mariaakhmadieva@yandex.ru>
2022-08-31 02:44:50 +0500

    ex36-done

--- homework05/ex36/Program.cs
+++ homework05/ex36/Program.cs
@@ -2,3 +2,16 @@
 // Найдите сумму элементов, стоящих на нечётных позициях.
 // [3, 7, 23, 12] -> 19
 // [-4, -6, 89, 6] -> 0
+
+int[] arr = new int[11];
+Random rnd = new Random();
+
+for (int i = 0; i < arr.Length; i++)
+{
+    arr[i] = rnd.Next(-100, 100);
+}
+
+Console.WriteLine("[{0}]", string.Join(", ", arr));
+
+var even = arr.Where((element, index) => index % 2 != 0);
+Console.WriteLine("Сумма элементов, стоящих на нечётных позициях: {1}", even.Count(), even.Sum());
